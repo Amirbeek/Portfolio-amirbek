8b8aab12f6743d15d7ccbbc467f7ccbf4683b292
amirbeek <amirbek.shomurodov01@gmail.com>
2025-01-07 05:37:31 +0000

    updated

--- src/project_data.js
+++ src/project_data.js
@@ -1,8 +1,7 @@
 export const projectData = [
     {
         title: "MiniDesk",
-        description: `TinyDesk is a web application that lets users manage bookmarks and boost their productivity
-                  with handy widgets.<br/> <br> This project was supervised by Alex Kuhn (ex-Apple Engineer) and led by me as 
+        description: `s.<br/> <br> This project was supervised by Alex Kuhn (ex-Apple Engineer) and led by me as 
                   a Lead Developer and Designer.<br><br>
         It was built with <span>GatsbyJS </span> & <span>Material UI</span> and deployed to Heroku.
     `,
@@ -14,22 +13,23 @@ export const projectData = [
             repoLink: "domain_url",
             PagePath: "/miniDesk",
         },
+
         highlight: "Feature Highlight",
         highlightDesc: `If you visited <a href='http://gooogle.com' >Github's homepage</a> recently, you probably 
                     saw their new, re-designed landing page... <span>hello</span>`,
         image: "https://janarosmonaliev.com/static/tinydesk-feature-2-8664c1aea65b568a24054b6c0c1ce1a2.png",
+        image_url:'',
         additional_images:['https://janarosmonaliev.com/static/tinydesk-feature-2-8664c1aea65b568a24054b6c0c1ce1a2.png',"https://janarosmonaliev.com/static/crime-data-feature-e5829c5eb36fffed279732775b967f93.png"]
     },
     {
-        title: "Crime Data Visualization",
-        description: `If you ever visited <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub's homepage</a>, 
-                  you will notice a 3D globe that visualizes the platform's current activity...<span>hello</span>`,
+        title: "Airport System",
+        description: ``,
         image: ["https://janarosmonaliev.com/static/crime-data-feature-e5829c5eb36fffed279732775b967f93.png"],
         links: {
             GitHubLink: "https://github.com",
             DomainLink: "https://uzbegim.co.uk",
             repoLink: "https://amirbeek.github.io",
-            PagePath: "/project",
+            PagePath: "/airport",
         },
         Project_Title: "APP",
         Project_type: "Web Application",
